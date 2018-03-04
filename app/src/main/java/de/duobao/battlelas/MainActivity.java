6663d0f76df638897612958453be3f392b3d77e0
David Hebbeker <dhebbeker@users.noreply.github.com>
2018-03-04 19:53:48 +0100

    Formatting code.
    * Set code indentation style to Allman.
    * Perform automatic:
    ** Reformat code.
    ** Rearrange code.
    ** Optimize imports.
    ** Update copyright.
    
    (cherry picked from commit eb0cca7)

--- app/src/main/java/de/duobao/battlelas/MainActivity.java
+++ app/src/main/java/de/duobao/battlelas/MainActivity.java
@@ -1,12 +1,14 @@
 package de.duobao.battlelas;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
