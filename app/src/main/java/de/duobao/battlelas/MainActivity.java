cf8994c83ec66bc4d361dd8d5139cbcbaa2f1ec2
David Hebbeker <dhebbeker@users.noreply.github.com>
2018-03-14 19:48:46 +0100

    First definitions to draw some basic stuff.

--- app/src/main/java/de/duobao/battlelas/MainActivity.java
+++ app/src/main/java/de/duobao/battlelas/MainActivity.java
@@ -3,12 +3,16 @@ package de.duobao.battlelas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import de.duobao.battlelas.game_engine.Game;
 import de.duobao.battlelas.gui.GameInterface;
 
 public class MainActivity extends AppCompatActivity
 {
-    private final GameInterface gameInterface = new GameInterface(new Game());
+    private final GameInterface gameInterface;
+
+    public MainActivity()
+    {
+        gameInterface = new GameInterface(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
--- app/src/main/java/de/duobao/battlelas/gui/GameInterface.java
+++ app/src/main/java/de/duobao/battlelas/gui/GameInterface.java
@@ -1,13 +1,45 @@
 package de.duobao.battlelas.gui;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.view.View;
+
 import de.duobao.battlelas.game_engine.Game;
 
-public class GameInterface
+public class GameInterface extends View
 {
     private final Game game;
 
-    public GameInterface(Game game)
+    /* @todo BEGIN: THIS SECTION IS FOR TESTS ONLY */
+    private final Drawable myGameObject;
+    private final Paint myCoolPaint;
+    /* @todo END  : THIS SECTION IS FOR TESTS ONLY */
+
+
+    public GameInterface(Context context)
     {
-        this.game = game;
+        super(context);
+        this.game = new Game(); /* @todo delete */
+        /* @todo this.game = game; */
+
+        /* create paint object to define how things are drawn */
+        myCoolPaint = new Paint();
+        myCoolPaint.setColor(Color.BLUE);
+
+
+        /* create something that can be drawn */
+        myGameObject = new ShapeDrawable(new OvalShape()); /* easy example */
+        myGameObject.setBounds(10, 10, 110, 110);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        myGameObject.draw(canvas);
     }
 }
